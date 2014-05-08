44d2436b5b0eddfab88d78def9aefa26528af721
Axzanaeb <Axzanaeb@gmail.com>
2014-05-08 21:40:48 +0100

    RD1 2.4

---
+++ TxRx/src/simulator/Main.java
@@ -0,0 +1,94 @@
+package simulator;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String[] sizesList = {"3200" ,"10", "100", "200", "300", "500", "750", "1000" };
+		String[] bnList = { "10000", "10000000" };
+		String[] dList = { "5000000", "100", "1000", "10000" };
+		String[] PbList = { "0.00001", "0.0001", "0.001", "0.01", "0.02" };
+		String[] TaxGnList = { "0.05", "0.10", "0.20", "0.50", "0.75", "0.90" };
+
+		final JComboBox streamSize = new JComboBox(sizesList);
+		final JComboBox binaryRate = new JComboBox(bnList);
+		final JComboBox d = new JComboBox(dList);
+		final JComboBox PbBox = new JComboBox(PbList);
+		final JComboBox TaxGn = new JComboBox(TaxGnList);
+		
+		JButton btn = new JButton("Start");
+  
+		
+		
+		JFrame frame = new JFrame("sup");
+		frame.setSize(new Dimension(500, 150));
+		frame.setLayout(new GridLayout(0,5, 0,20));
+
+
+		JLabel l1=new JLabel("Data Size", JLabel.CENTER);
+		JLabel l2=new JLabel("Bin. rate", JLabel.CENTER);
+		JLabel l3=new JLabel("Distance", JLabel.CENTER);
+		JLabel l4=new JLabel("Pb", JLabel.CENTER);
+		JLabel l5=new JLabel("Tax. Gen.", JLabel.CENTER);
+		
+		l1.setFont(new Font("Serif", Font.BOLD, 14));
+		l2.setFont(new Font("Serif", Font.BOLD, 14));
+		l3.setFont(new Font("Serif", Font.BOLD, 14));
+		l4.setFont(new Font("Serif", Font.BOLD, 14));
+		l5.setFont(new Font("Serif", Font.BOLD, 14));
+		
+		frame.add(l1);
+		frame.add(l2);
+		frame.add(l3);
+		frame.add(l4);
+		frame.add(l5);
+		
+		frame.add(streamSize);
+		frame.add(binaryRate);
+		frame.add(d);
+		frame.add(PbBox);
+		frame.add(TaxGn);
+		
+		
+	
+		
+		frame.add(new JLabel());
+		frame.add(new JLabel());
+		frame.add(btn);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int size=Integer.parseInt((String) streamSize.getSelectedItem());
+				double rate= Double.parseDouble((String) binaryRate.getSelectedItem());
+				double dist = Double.parseDouble((String) d.getSelectedItem());
+				double Pb= Double.parseDouble((String) PbBox.getSelectedItem());
+				double TxG = Double.parseDouble((String) TaxGn.getSelectedItem());
+				
+				
+				TxRxSystem TxRx = new TxRxSystem(size, rate, dist, Pb, TxG);
+				TxRx.init();
+			}
+		});
+
+		
+
+	}
+
+}
---
+++ TxRx/src/simulator/Receiver.java
@@ -0,0 +1,106 @@
+package simulator;
+
+import java.util.Random;
+
+import simulator.TxRxEvent.TxRxEventType;
+
+/**
+ * 
+ * Esta classe implementa o comportamento de um receptor..<br>
+ * <br>
+ * Possuí métodos para:<br>
+ * - Iniciar a recepçao de dados {@link Sender.Data}.<br>
+ * - Terminar a recepçao de dados {@link Sender.Data}.<br>
+ * 
+ * @author Grupo de Redes do ISCTE (20012/13)
+ * 
+ */
+
+public class Receiver {
+	public enum State {
+		WAIT, RX
+	};
+
+	private State state;
+	private double Peb;
+	private int m;
+	private double PROB_CORR;
+	private double PROB_ERROR;
+
+	public Receiver(double Peb, int DATA_SIZE) {
+		state = State.WAIT;
+
+		PROB_ERROR = 1 - Math.pow((1 - Peb), (26 + 8));
+		m = DATA_SIZE;
+		this.Peb = Peb;
+		PROB_CORR = 1 - Math.pow((1 - Peb), (26 + 8));
+		PROB_CORR = 0.8;
+	}
+
+	/**
+	 * Inicia a recepção de dados.
+	 * 
+	 * @param Os
+	 *            dados recebidos.
+	 */
+	public void startRX(Data data) {
+
+		state = State.RX;
+
+		// Update statistics
+
+		// Output
+		String s = "[Receiver@";
+		s = s + Simulator.getClock() + " Start RX Data ID: " + data.getID()
+				+ "]";
+		Simulator.debug(s);
+
+		s = "" + Simulator.getClock() + "\t" + "Srx" + "\t" + data.getID()
+				+ "\t";
+		s = s + data.getTimeStamp() + "\t" + "-" + "\t" + "-" + "\t"
+				+ Simulator.getClock() + "\t" + "-" + "\t" + "-" + "\t" + "-";
+		Simulator.data(s);
+
+	}
+
+	public void stopRX(Data data) {
+
+		state = State.WAIT;
+
+		// Update statistics
+		/*
+		 * Ao tempo total no sistema, delaySys é adicionado o tempo passado no
+		 * sistema (tempoActual-tempoGeração) da trama que acabou de ser
+		 * recebida
+		 */
+		TxRxSystem.delaySys += Simulator.getClock() - data.getTimeStamp();
+
+		// Output
+		String s = "[Receiver@";
+		s = s + Simulator.getClock() + " Stop RX Data ID (State=" + state
+				+ "): " + data.getID() + "]";
+		Simulator.debug(s);
+
+		s = "" + Simulator.getClock() + "\t" + "Erx" + "\t" + data.getID()
+				+ "\t";
+		s = s + data.getTimeStamp() + "\t" + "-" + "\t" + "-" + "\t" + "-"
+				+ "\t" + Simulator.getClock() + "\t" + "-" + "\t" + "-";
+		Simulator.data(s);
+
+		if (Successful()) {
+			Simulator.addEvent(new TxRxEvent(Simulator.getClock(),
+					TxRxEventType.ACK, data));
+		}
+
+	}
+
+	private boolean Successful() {
+		/*
+		 * Random rand = new Random(); double randInt = rand.nextDouble(); if
+		 * (randInt > PROB_CORR) { System.out.println("\n !!!!!  FODEU  !!!!!  "
+		 * + randInt+"\n"); return false; }
+		 */
+
+		return true;
+	}
+}
